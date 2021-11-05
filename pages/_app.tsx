d0e71335a416c716e57a67122f4255fe707815df
cmacdougall12 <cammacdougall12@gmail.com>
2021-11-05 15:52:41 -0600

    add navbar to app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,14 @@
 import "../styles/globals.css";
 import { GetServerSideProps } from "next";
+import NavBar from "../components/NavBar";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <NavBar />
+      <Component {...pageProps} />;
+    </>
+  );
 }
 
 export default MyApp;
