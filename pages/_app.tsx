6998464b87ad0c9fb08f0f805cf1af9022b9fea7
cmacdougall12 <cammacdougall12@gmail.com>
2021-11-10 10:58:47 -0700

    custom username

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,16 +1,18 @@
 import "../styles/globals.css";
 import { GetServerSideProps } from "next";
-import NavBar from "../components/NavBar";
+import Navbar from "../components/NavBar";
 import { Toaster } from "react-hot-toast";
 import { UserContext } from "../lib/context";
 import { useUserData } from "../lib/hooks";
 
+
 function MyApp({ Component, pageProps }) {
+  
   const userData = useUserData();
 
   return (
     <UserContext.Provider value={userData}>
-      <NavBar />
+      <Navbar />
       <Component {...pageProps} />
       <Toaster />
     </UserContext.Provider>
