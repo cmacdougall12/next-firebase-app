26b76274fd11f42e2c6e16e17d045b59236a8c5c
cmacdougall12 <cammacdougall12@gmail.com>
2021-11-12 09:11:37 -0700

    SSR and SEO features added to app

--- lib/hooks.ts
+++ lib/hooks.ts
@@ -1,7 +1,7 @@
-import { auth, firestore } from '../lib/firebase';
-import { useEffect, useState } from 'react';
-import { useAuthState } from 'react-firebase-hooks/auth';
-// Test Remote
+import { auth, firestore } from "../lib/firebase";
+import { useEffect, useState } from "react";
+import { useAuthState } from "react-firebase-hooks/auth";
+
 // Custom hook to read  auth record and user profile doc
 export function useUserData() {
   const [user] = useAuthState(auth);
@@ -12,7 +12,7 @@ export function useUserData() {
     let unsubscribe;
 
     if (user) {
-      const ref = firestore.collection('users').doc(user.uid);
+      const ref = firestore.collection("users").doc(user.uid);
       unsubscribe = ref.onSnapshot((doc) => {
         setUsername(doc.data()?.username);
       });
--- pages/enter.tsx
+++ pages/enter.tsx
@@ -1,12 +1,15 @@
-import React, { useContext, useState, useEffect, useCallback } from "react";
+import { auth, firestore, googleAuthProvider } from "../lib/firebase";
 import { UserContext } from "../lib/context";
-import { auth, googleAuthProvider } from "../lib/firebase";
-import { firestore } from "../lib/firebase";
-import { debounce } from "lodash.debounce";
 
-export default function EnterPage() {
+import { useEffect, useState, useCallback, useContext } from "react";
+import debounce from "lodash.debounce";
+
+export default function Enter(props) {
   const { user, username } = useContext(UserContext);
 
+  // 1. user signed out <SignInButton />
+  // 2. user signed in, but missing username <UsernameForm />
+  // 3. user signed in, has username <SignOutButton />
   return (
     <main>
       {user ? (
@@ -22,21 +25,25 @@ export default function EnterPage() {
   );
 }
 
+// Sign in with Google button
 function SignInButton() {
   const signInWithGoogle = async () => {
     await auth.signInWithPopup(googleAuthProvider);
   };
+
   return (
-    <button className="btn-google">
-      <img src={"/google.png"} width={30} /> Sign in with Google
+    <button className="btn-google" onClick={signInWithGoogle}>
+      <img src={"/google.png"} width="30px" /> Sign in with Google
     </button>
   );
 }
 
+// Sign out button
 function SignOutButton() {
   return <button onClick={() => auth.signOut()}>Sign Out</button>;
 }
 
+// Username form
 function UsernameForm() {
   const [formValue, setFormValue] = useState("");
   const [isValid, setIsValid] = useState(false);
@@ -44,14 +51,31 @@ function UsernameForm() {
 
   const { user, username } = useContext(UserContext);
 
-  useEffect(() => {
-    checkUsername(formValue);
-  }, [formValue]);
+  const onSubmit = async (e) => {
+    e.preventDefault();
+
+    // Create refs for both documents
+    const userDoc = firestore.doc(`users/${user.uid}`);
+    const usernameDoc = firestore.doc(`usernames/${formValue}`);
+
+    // Commit both docs together as a batch write.
+    const batch = firestore.batch();
+    batch.set(userDoc, {
+      username: formValue,
+      photoURL: user.photoURL,
+      displayName: user.displayName,
+    });
+    batch.set(usernameDoc, { uid: user.uid });
+
+    await batch.commit();
+  };
 
   const onChange = (e) => {
+    // Force form value typed in form to match correct format
     const val = e.target.value.toLowerCase();
     const re = /^(?=[a-zA-Z0-9._]{3,15}$)(?!.*[_.]{2})[^_.].*[^_.]$/;
 
+    // Only set form value if length is < 3 OR it passes regex
     if (val.length < 3) {
       setFormValue(val);
       setLoading(false);
@@ -65,12 +89,20 @@ function UsernameForm() {
     }
   };
 
+  //
+
+  useEffect(() => {
+    checkUsername(formValue);
+  }, [formValue]);
+
+  // Hit the database for username match after each debounced change
+  // useCallback is required for debounce to work
   const checkUsername = useCallback(
     debounce(async (username) => {
       if (username.length >= 3) {
         const ref = firestore.doc(`usernames/${username}`);
         const { exists } = await ref.get();
-        console.log("Firestore read executed");
+        console.log("Firestore read executed!");
         setIsValid(!exists);
         setLoading(false);
       }
@@ -78,33 +110,17 @@ function UsernameForm() {
     []
   );
 
-  const onSubmit = async (e) => {
-    e.preventDefault();
-
-    const userDoc = firestore.doc(`user/${user.uid}`);
-    const usernameDoc = firestore.doc(`usernames/${formValue}`);
-
-    const batch = firestore.batch();
-    batch.set(userDoc, {
-      username: formValue,
-      photoURL: user.photoURL,
-      displayName: user.displayName,
-    });
-    batch.set(usernameDoc, { uid: user.uid });
-    await batch.commit();
-  };
-
   return (
     !username && (
       <section>
-        <h3>Chose Username</h3>
+        <h3>Choose Username</h3>
         <form onSubmit={onSubmit}>
           <input
             name="username"
-            placeholder="username"
+            placeholder="myname"
             value={formValue}
             onChange={onChange}
-          ></input>
+          />
           <UsernameMessage
             username={formValue}
             isValid={isValid}
@@ -113,10 +129,13 @@ function UsernameForm() {
           <button type="submit" className="btn-green" disabled={!isValid}>
             Choose
           </button>
+
           <h3>Debug State</h3>
           <div>
-            Username: {formValue} <br />
-            Loading : {loading.toString()} <br />
+            Username: {formValue}
+            <br />
+            Loading: {loading.toString()}
+            <br />
             Username Valid: {isValid.toString()}
           </div>
         </form>
@@ -129,7 +148,7 @@ function UsernameMessage({ username, isValid, loading }) {
   if (loading) {
     return <p>Checking...</p>;
   } else if (isValid) {
-    return <p>{username} is available!</p>;
+    return <p className="text-success">{username} is available!</p>;
   } else if (username && !isValid) {
     return <p className="text-danger">That username is taken!</p>;
   } else {
