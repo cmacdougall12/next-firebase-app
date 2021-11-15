93f55cb8d8081fc1a082f8bc112430b5ebe77cc9
cmacdougall12 <cammacdougall12@gmail.com>
2021-11-15 07:51:14 -0700

    edit posts and update to use hookform v7

--- components/AuthCheck.tsx
+++ components/AuthCheck.tsx
@@ -2,8 +2,9 @@ import Link from "next/link";
 import { useContext } from "react";
 import { UserContext } from "../lib/context";
 
+// Component's children only shown to logged-in users
 export default function AuthCheck(props) {
-  const { username } = UserContext;
+  const { username } = useContext(UserContext);
 
   return username
     ? props.children
