7aa8e6637ab07278f06fa241da8249d134e5a1d2
cmacdougall12 <cammacdougall12@gmail.com>
2021-11-15 08:07:33 -0700

    image uploader added to posts

--- pages/admin/[slug].tsx
+++ pages/admin/[slug].tsx
@@ -1,7 +1,7 @@
 import styles from "../../styles/Admin.module.css";
 import AuthCheck from "../../components/AuthCheck";
 import { firestore, auth, serverTimestamp } from "../../lib/firebase";
-// import ImageUploader from "../../components/Ima";
+import ImageUploader from "../../components/ImageUploader";
 
 import { useState } from "react";
 import { useRouter } from "next/router";
@@ -99,7 +99,7 @@ function PostForm({ defaultValues, postRef, preview }) {
       )}
 
       <div className={preview ? styles.hidden : styles.controls}>
-        {/* <ImageUploader /> */}
+        <ImageUploader />
 
         <textarea
           {...register("content", {
