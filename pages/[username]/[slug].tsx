f5039497abf3becc7f7ce2680a956a401f2094fe
cmacdougall12 <cammacdougall12@gmail.com>
2021-11-15 08:50:22 -0700

    fixed heart bug

--- pages/[username]/[slug].tsx
+++ pages/[username]/[slug].tsx
@@ -1,10 +1,14 @@
 import styles from "../../styles/Post.module.css";
 import PostContent from "../../components/PostContent";
-import { firestore, getUserWithUsername, postToJSON } from "../../lib/firebase";
-import { useDocumentData } from "react-firebase-hooks/firestore";
 import HeartButton from "../../components/HeartButton";
 import AuthCheck from "../../components/AuthCheck";
+import Metatags from "../../components/Metatags";
+import { UserContext } from "../../lib/context";
+import { firestore, getUserWithUsername, postToJSON } from "../../lib/firebase";
+
 import Link from "next/link";
+import { useDocumentData } from "react-firebase-hooks/firestore";
+import { useContext } from "react";
 
 export async function getStaticProps({ params }) {
   const { username, slug } = params;
@@ -22,7 +26,7 @@ export async function getStaticProps({ params }) {
 
   return {
     props: { post, path },
-    revalidate: 5000,
+    revalidate: 100,
   };
 }
 
@@ -53,13 +57,21 @@ export default function Post(props) {
 
   const post = realtimePost || props.post;
 
+  const { user: currentUser } = useContext(UserContext);
+
   return (
     <main className={styles.container}>
+      <Metatags title={post.title} description={post.title} />
+
       <section>
         <PostContent post={post} />
       </section>
 
       <aside className="card">
+        <p>
+          <strong>{post.heartCount || 0} 🤍</strong>
+        </p>
+
         <AuthCheck
           fallback={
             <Link href="/enter">
@@ -69,6 +81,12 @@ export default function Post(props) {
         >
           <HeartButton postRef={postRef} />
         </AuthCheck>
+
+        {currentUser?.uid === post.uid && (
+          <Link href={`/admin/${post.slug}`}>
+            <button className="btn-blue">Edit Post</button>
+          </Link>
+        )}
       </aside>
     </main>
   );
